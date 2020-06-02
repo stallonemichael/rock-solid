cbdc6291806ee844401b61e85e2555e50271a312
Michael Stallone <michael.stallone1@gmail.com>
2020-06-02 10:12:23 -0400

    updated contact page to include a message and company phone number above the form as the phone number was not displayed anywhere but the footer

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -4,15 +4,30 @@ import './contact.scss';
 
 import ContactUs from '../../components/contact-us/contact-us';
 import Footer from '../../components/footer/footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 
 const ContactPage = () => (
   <div className='contact-page'>
-    <div className='contact-text'>
+    <div className='hero-text'>
       <h1>WELCOME HOME</h1>
       <span>Contact Us Today</span>
     </div>
 
-    <div className='contact-image'></div>
+    <div className='hero-image'></div>
+    <div className='contact-info'>
+      <h3>
+        Thank you for considering Rock Solid Brick &amp; Masonry for your next
+        masonry project. We look forward to hearing from you!
+      </h3>
+    </div>
+    <div className='contact-phone'>
+      <FontAwesomeIcon icon={faPhoneAlt} size='lg' />
+      <a className='phone-number' href='tel:1-631-872-3865'>
+        631-872-3865
+      </a>
+    </div>
+
     <ContactUs />
     <Footer />
   </div>
